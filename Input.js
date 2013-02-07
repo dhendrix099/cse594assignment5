8cc0a77ed92013a70e4782c42b0fbc7b2bee4658
Dustin Hendrix <hendrixd@coyote.csusb.edu>
2013-02-06 19:35:19 -0800

    More tweaking

--- Input.js
+++ Input.js
@@ -1,94 +1,89 @@
 // INPUT: Gather input and update rotations/velocity/etc.  (This is through the use of github repository 'kibo')
 // https://github.com/marquete/kibo.git
 
-
-// PC INPUTS
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-console.log('Mobile device recognized');
-// MOBILE INPUTS
-$.mobile.defaultPageTransition   = 'none';
-$.mobile.defaultDialogTransition = 'none';
-$.mobile.buttonMarkup.hoverDelay = 0;
-
 $(document).ready(function() {
-  $(document).live('swiperight', function(event, data) {
-    event.stopImmediatePropagation(); // to prevent "Double Event Firing" of a swipe event's function
-    if(gameRunning && allowInput){
-	  clearBlock(ctx, activeBlock);
-	  activeBlock.moveRight();
-	  activeBlock.draw();
-    }
-  });
-  $(document).live('swipeleft', function(event, data) {
-    event.stopImmediatePropagation(); // to prevent "Double Event Firing" of a swipe event's function
-    if(gameRunning && allowInput){
-	  clearBlock(ctx, activeBlock);
-      activeBlock.moveLeft();
-	  activeBlock.draw();
-    }
-  });
-  $(document).live('tap', function(event, data) {
-    event.stopImmediatePropagation(); // to prevent "Double Event Firing" of a swipe event's function
-    if(gameRunning && allowInput){
-	  clearBlock(ctx, activeBlock);
-	  activeBlock.rotate();
-	  activeBlock.draw();
-    }
-  });
-  $(document).live('taphold', function(event, data) {
-    event.stopImmediatePropagation(); // to prevent "Double Event Firing" of a swipe event's function
-    if(gameRunning && allowInput){
-	  activeBlock.fullDrop();
-	  allowInput = false;
-    }
-  });
-});
-}
-else{
-console.log('Non-mobile device');
-var k = new Kibo();
-k.down(['left'], function () {
-  if(gameRunning && allowInput){
-	clearBlock(ctx, activeBlock);
-	activeBlock.moveLeft();
-	activeBlock.draw();
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { // MOBILE INPUTS
+    console.log('Mobile device recognized');
+    $.mobile.defaultPageTransition   = 'none';
+    $.mobile.defaultDialogTransition = 'none';
+    $.mobile.buttonMarkup.hoverDelay = 0;
+    $(document).bind('swiperight', function(event, data) {
+      event.stopImmediatePropagation(); // to prevent "Double Event Firing" of a swipe event's function
+      inpRight();
+    });
+    $(document).bind('swipeleft', function(event, data) {
+      event.stopImmediatePropagation();
+      inpLeft();
+    });
+    $(document).bind('tap', function(event, data) {
+      event.stopImmediatePropagation();
+      inpRotate();
+    });
+    $(document).bind('taphold', function(event, data) {
+      event.stopImmediatePropagation();
+      inpDrop();
+    });
+  }
+  else { // PC INPUTS
+    console.log('Non-mobile device');
+    var k = new Kibo();
+    k.down(['left'], inpLeft);
+    k.down(['right'], inpRight);
+    k.down(['down'], inpDown);
+    k.down(['up'], inpRotate);
+    k.down(['p'], inpPause);
+    k.down(['space'], inpDrop);
+    k.down(['enter'], inpSwap);
   }
 });
-k.down(['right'], function () {
+
+// Input definitions
+inpRight = function() {
   if(gameRunning && allowInput){
 	clearBlock(ctx, activeBlock);
 	activeBlock.moveRight();
 	activeBlock.draw();
   }
-});
-k.down(['down'], function () {
+}
+inpLeft = function() {
   if(gameRunning && allowInput){
 	clearBlock(ctx, activeBlock);
-	activeBlock.moveDown();
+	activeBlock.moveLeft();
 	activeBlock.draw();
   }
-});
-k.down(['up'], function () {
+}
+inpRotate = function() {
   if(gameRunning && allowInput){
 	clearBlock(ctx, activeBlock);
 	activeBlock.rotate();
 	activeBlock.draw();
   }
-});
-k.down(['p'], function () {
+}
+inpDown = function() {
   if(gameRunning && allowInput){
-	gameRunning = false;
- }
-});
-k.down(['space'], function () {
+	clearBlock(ctx, activeBlock);
+	activeBlock.moveDown();
+	activeBlock.draw();
+  }
+}
+inpDrop = function() {
   if(gameRunning && allowInput){
 	activeBlock.fullDrop();
 	allowInput = false;
- }
-});
-k.down(['enter'], function () {
+  }
+}
+inpSwap = function() {
   if(gameRunning && allowInput){
 	swapBlocks();
- }
-});
+  }
+}
+inpPause = function() {
+  console.log('pause');
+  if(gameRunning && allowInput){
+	gameRunning = false;
+  }
+  else {
+	gameRunning = true;
+	setTimeout("tetrisLoop()", INTERVAL);
+  }
 }
